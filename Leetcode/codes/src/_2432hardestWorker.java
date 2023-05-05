7db3ec2c3bb85844c42e68e0d31a560bdd43bebe
JiyuanWangYo <anonymous.z@yahoo.com>
2023-05-05 21:14:01 +0800

    Leetcode

---
+++ Leetcode/codes/src/_2432hardestWorker.java
@@ -0,0 +1,22 @@
+public class _2432hardestWorker {
+    static class Solution {
+        public int hardestWorker(int n, int[][] logs) {
+            int ans = 0;
+            int last = 0, mx = 0;
+            for (int[] log : logs) {
+                int uid = log[0], t = log[1];
+                t -= last;
+                if (mx < t || (mx == t && ans > uid)) {
+                    ans = uid;
+                    mx = t;
+                }
+                last += t;
+            }
+            return ans;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
